e66ea24715b73b8b115b30e3f300ba323dfbbe0d
Administrator <admin@example.com>
2018-10-04 10:31:13 +0700

    Update Activity_passpod.js

--- cypress/integration/Activity_passpod.js
+++ cypress/integration/Activity_passpod.js
@@ -3,24 +3,24 @@ describe('Activity', function() {
         cy.server();
         cy.route('**/attractions/**/short?**').as("getAttraction")
         // go to check the path
-        cy.visit(Cypress.env('BASE_URL_LIVE'));
+        cy.visit(Cypress.env('BASE_URL_LIVE')).log('Open Homepage');
 
-        cy.get(':nth-child(2) > :nth-child(1) > .member-link').click()
-        cy.get('div.slick-slide div.dest-title:contains("Hong Kong Disneyland")').click()
-        cy.get('div.top-float-box #btnFloat').click()
-        cy.get('.orange-input').click()
+        cy.get(':nth-child(2) > :nth-child(1) > .member-link').click().log('Click 1')
+        cy.get('div.slick-slide div.dest-title:contains("Hong Kong Disneyland")').click().log('Click 2')
+        cy.get('div.top-float-box #btnFloat').click().log('Click 3')
+        cy.get('.orange-input').click().log('Click 4')
         
         //for set and get today date
         var d = new Date();
         d.setDate(d.getDate() + 4);
-        cy.get('.react-datepicker__day--outside-month:contains('+d.getDate()+'):not(.react-datepicker__day--disabled)').click()
+        cy.get('.react-datepicker__day--outside-month:contains('+d.getDate()+'):not(.react-datepicker__day--disabled)').click().log('Click 5')
 
-        cy.get(':nth-child(1) > .package-box-top > .row > .col-lg-5 > .col-wrap > #btnSelect').click()
-        cy.get('#dropdown0 > .package-box-down > .package-form-wrap > .row > :nth-child(3) > .user-input-form > .form-group > .form-control').click()
-        cy.get('#dropdown0 > .package-box-down > .package-form-wrap > .row > :nth-child(3) > .user-input-form > .form-group > .user-wrap > :nth-child(1) > .nbr-form-wrap > .max').click()
-        cy.get('#dropdown0 > .package-box-down > .package-form-wrap > .row > :nth-child(3) > .user-input-form > .form-group > .user-wrap > .btn-user > #btnOk').click()
+        cy.get(':nth-child(1) > .package-box-top > .row > .col-lg-5 > .col-wrap > #btnSelect').click().log('Click 6')
+        cy.get('#dropdown0 > .package-box-down > .package-form-wrap > .row > :nth-child(3) > .user-input-form > .form-group > .form-control').click().log('Click 7')
+        cy.get('#dropdown0 > .package-box-down > .package-form-wrap > .row > :nth-child(3) > .user-input-form > .form-group > .user-wrap > :nth-child(1) > .nbr-form-wrap > .max').click().log('Click 8')
+        cy.get('#dropdown0 > .package-box-down > .package-form-wrap > .row > :nth-child(3) > .user-input-form > .form-group > .user-wrap > .btn-user > #btnOk').click().log('Click 9')
 
-        cy.get('.btn-wrap > .orange:visible').click()
+        cy.get('.btn-wrap > .orange:visible').click().log('Click 10')
         
         cy.wait(2000)
         cy.get('form').within(() =>{
