03d73d616569d2eb0222573593b569e83424e604
Tiara <tiara@skyshi.io>
2018-10-30 16:16:15 +0700

    perubahan

--- cypress/integration/activityToCartSt.js
+++ cypress/integration/activityToCartSt.js
@@ -7,7 +7,7 @@ describe('Activity to cart before login', function() {
         cy.visit(Cypress.env('BASE_URL')).log('Open Homepage');
 
         cy.get(':nth-child(2) > :nth-child(1) > .member-link').click().log('Click 1')
-        cy.get('div.slick-slide div.dest-title:eq(0)').click().log('Click 2')
+        cy.get('div.slick-slide div.dest-title:eq(3)').click().log('Click 2')
         cy.get('div.top-float-box #btnFloat').click().log('Click 3')
         cy.get('.orange-input').click().log('Click 4')
         
@@ -17,7 +17,7 @@ describe('Activity to cart before login', function() {
         cy.get('.react-datepicker__day:contains('+d.getDate()+'):not(.react-datepicker__day--disabled):eq(0)').click().log('Click 5')
 
         cy.get(':nth-child(1) > .package-box-top > .row > .col-lg-5 > .col-wrap > #btnSelect').click().log('Click 6')
-        cy.get('#dropdown0 :nth-child(2) > .form-group > .form-control').select('09:00:00')
+        // cy.get('#dropdown0 :nth-child(2) > .form-group > .form-control').select('19:15:00')
         cy.get('#dropdown0 .user-input-form .form-group .form-control').click()
         cy.get('#dropdown0 :nth-child(1) > .nbr-form-wrap > .max').click().log('Click 7')
         cy.get('#dropdown0 :nth-child(1) > .nbr-form-wrap > .max').click().log('Click 8')
@@ -35,7 +35,7 @@ describe('Activity to cart before login', function() {
       cy.get('form > .btn-orange').click()
       
       cy.wait(1000)
-      cy.get(':nth-child(5) > .input-group > .form-control').type('8765432190')
+      cy.get(':nth-child(5) > .input-group > .form-control').type('87654321900')
       cy.get('.panel-btn-payment > .btn').click()
 
       cy.wait(1000)
